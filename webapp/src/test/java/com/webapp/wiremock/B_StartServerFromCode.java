da755934bb35a6eee654b6629f7ac6f68e49b46f
NarayanaGolla <narayana.golla.aws@gmail.com>
2025-08-26 16:44:15 +0530

    added cucumber

--- webapp/src/test/java/com/webapp/wiremock/B_StartServerFromCode.java
+++ webapp/src/test/java/com/webapp/wiremock/B_StartServerFromCode.java
@@ -1,7 +1,6 @@
 package com.webapp.wiremock;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -22,24 +21,24 @@ public class B_StartServerFromCode {
     server.start();
     WireMock.configureFor(HOST, PORT);
 
-    ResponseDefinitionBuilder mockResponse = new ResponseDefinitionBuilder();
-    mockResponse.withStatus(201);
-    mockResponse.withStatusMessage("Hello Guys");
-    mockResponse.withHeader("Content-Type", "text/json");
-    mockResponse.withHeader("token", "111");
-    mockResponse.withHeader("Set-Cookie", "111");
-    mockResponse.withHeader("Set-Cookie", "111");
-    mockResponse.withBody("text to put in the Body");
-
-    WireMock.givenThat(WireMock.get("/emps/1").willReturn(mockResponse));
+    //    ResponseDefinitionBuilder mockResponse = new ResponseDefinitionBuilder();
+    //    mockResponse.withStatus(201);
+    //    mockResponse.withStatusMessage("Hello Guys");
+    //    mockResponse.withHeader("Content-Type", "text/json");
+    //    mockResponse.withHeader("token", "111");
+    //    mockResponse.withHeader("Set-Cookie", "111");
+    //    mockResponse.withHeader("Set-Cookie", "111");
+    //    mockResponse.withBody("text to put in the Body");
+    //
+    //    WireMock.givenThat(WireMock.get("/emps/1").willReturn(mockResponse));
   }
 
   // test case 1
   @Test
   public void closeServer() {
-    // if (server.isRunning() && null != server) {
-    //    server.shutdown();
-    // }
+    if (server.isRunning() && null != server) {
+      server.shutdown();
+    }
   }
 
   @AfterClass
